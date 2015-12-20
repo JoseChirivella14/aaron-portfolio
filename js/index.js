eaa6bd7da344016942ae02048f0b48bbd9254fe9
Jose Chirivella <jrchg1403@gmail.com>
2015-12-19 22:06:52 -0500

    js perfectly working

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-	console.log('hi');
+$(function() {
+    var heroImages = $('.hero-images');
+    var contentIndex = -1;
+
+    function showNextContent() {
+        ++contentIndex;
+        heroImages.eq(contentIndex % heroImages.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextContent);
+    }
+    showNextContent();
 });
